dfeedd6b7a007748a421d684955a78cae9f13f5c
Adiee <97231578+AdieDev@users.noreply.github.com>
2023-07-16 19:38:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ client.on('ready', async () => {
 
   console.log(`${client.user.tag} - titid kudanil`
   )
-  bitly_axel = "https://bit.ly/3oQZWb0"
+  bitly_axel = "https://bit.ly/3oQZWb0"//jangan diubah!!
 
   const r = new Discord.RichPresence()
-    .setApplicationId('937827593904852992')
+    .setApplicationId('937827593904852998')
     .setType('PLAYING') 
-    .setURL('https://bit.ly/3oQZWb0')
-    //.setState('F Studio')
+    .setURL('https://bit.ly/3oQZWb0')//jangan diubah!!
+    .setState('F Studio')
     .setName('Google Chrome')
-    //.setDetails('Editing fold.js')
+    .setDetails('Editing fold.js')
 
 
     .setStartTimestamp(Date.now())
-    //.setAssetsLargeImage('https://cdn.discordapp.com/attachments/973124172479856726/1127194031659692043/images.jpg')
-    //.setAssetsLargeText('images')
-    //.setAssetsSmallImage('https://cdn.discordapp.com/attachments/975337808422137877/1126475448768016446/javascript-39413.png')
-    //.setAssetsSmallText('javascript-39413')
-        //.addButton('Connect', "https://cfx.re/join/r35px8")
-    //.addButton('Discord',"https://discord.gg/kaVBxkRVkP")
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/973124172479856726/1127194031659692043/images.jpg')//wajib copy link dari discord!!
+    .setAssetsLargeText('images')
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/975337808422137877/1126475448768016446/javascript-39413.png')
+    .setAssetsSmallText('javascript-39413')
+    .addButton('Connect', "https://cfx.re/join/r35px8")
+    .addButton('Discord',"https://discord.gg/kaVBxkRVkP")
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" });
+  client.user.setPresence({ status: "dnd" });//custom dengan idle,dnd
 })
 require('dotenv').config();
 keepAlive();
-client.login(process.env.token) //Your account token
+client.login(process.env.token) //Your account tokenm, via kiwi browser
 
 // man put ur token in secrets
